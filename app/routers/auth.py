41d552b859ac4bfdfee68757e3e6ffd6976200de
yao qu <yao.qu@outlook.com>
2022-10-01 15:19:02 +0100

    import fix

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,7 +8,7 @@ router = APIRouter(tags=["Authentication"])
 
 
 @router.post('/login', response_model=schemas.Token)
-def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
     user = db.query(models.User).filter(
         models.User.email == user_credentials.username).first()
