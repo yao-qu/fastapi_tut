54e11dc3a1611f17672901687159db609120b8a6
yao qu <yao.qu@outlook.com>
2022-10-01 15:08:07 +0100

    imports fixed

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
-from config import settings
-from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import time
+from .config import settings
 
-import sys
-sys.path.append("..")
+
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
