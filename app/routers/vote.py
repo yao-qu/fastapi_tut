f33fcef4c8d1b8ba0a4535933f22575e1a362c06
yao qu <yao.qu@outlook.com>
2022-10-01 15:22:05 +0100

    import fix

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -6,7 +6,7 @@ router = APIRouter(prefix="/vote", tags=['votes'])
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def votes(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def votes(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
     if not post:
         print("Post not found")
